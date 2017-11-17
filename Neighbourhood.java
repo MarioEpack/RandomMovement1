edd09ba8397e7bb874bab52755ae8f814ff909e0
Mario Alina <Mario.alina11@gmail.com>
2017-11-17 18:29:28 +0100

    Update Neighbourhood.java

--- Neighbourhood.java
+++ Neighbourhood.java
@@ -1,18 +1,36 @@
 package Zadanie7_8;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+/**
+ * This class is used to draw the customized JPanels in a certain way JFrame in
+ * OpilecGui is filled with these
+ * 
+ * @author Mario Alina
+ *
+ */
 public class Neighbourhood extends JPanel {
 
-	private Opilec opilec = new Opilec();
+	private boolean green;
+
+	public void draw_green(boolean green) {
+		this.green = green;
+	}
 
 	@Override
 	public void paintComponent(Graphics g) {
-		g.drawRect(0, 0, 50, 50);
-		while (this.opilec.is_visited(this.opilec.get_position_index_x(), this.opilec.get_position_index_y()) == true) {
-			g.drawOval(10, 10, 10, 10);
+		System.out.println("je: " + this.green);
+		if (green == true) {
+			g.setColor(Color.GREEN);
+		} else {
+			g.setColor(Color.ORANGE);
+
 		}
+		g.fillRect(0, 0, 50, 50);
+
 	}
+
 }
